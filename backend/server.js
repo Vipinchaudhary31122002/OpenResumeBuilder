5b87215007ddb2d2b3df11d48ffdab68df914a81
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-01-09 20:39:59 +0530

    try to serve react app whenever there will be a request to the nodejs app and removed type module from backend package.json

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
-import express from "express";
+const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Serve the React app on all routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+// });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
