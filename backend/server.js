a2b16b0e21cf2744c5e0e35a487224216500675f
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-01-18 02:14:27 +0530

    implemented client side routing in frontend

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
 
 app.get("/admin", (req, res) => {
   res.send("admin page Hello World!");
@@ -16,11 +13,18 @@ app.get("/client", (req, res) => {
 });
 
 app.get("/client1", (req, res) => {
-  res.send("Client page Hello World!");
+  res.send("Client1 page Hello World!");
 });
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+// app.get("/", (req, res) => {
+//   res.json({
+//      'name': 'demon'
+//    })
+//    res.send("Hello World!");
+// });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
-
   return (
     <>
-      <h1>Frontend is working</h1>
+      <Routes>
+        <Route exact path="/">
+          <Route index element={<Header info="Midguard" />} />
+          <Route
+            path="hell"
+            element={<Header info="The Hell of the universe" />}
+          />
+          <Route
+            path="heaven"
+            element={<Header info="The Heaven of the universe" />}
+          />
+          <Route path="*" element={<Header info="404 page not found" />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
