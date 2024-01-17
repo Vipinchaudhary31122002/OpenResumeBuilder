b896f5889987eb71d06c5b5ec5371c3826cf5e56
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-01-17 22:56:13 +0530

    added the code to serve the react application

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -17,6 +18,8 @@ app.get("/client", (req, res) => {
 app.get("/client1", (req, res) => {
   res.send("Client page Hello World!");
 });
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
