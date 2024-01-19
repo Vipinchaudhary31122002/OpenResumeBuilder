3f4c8fe4e61e31e84a9f56eb41b0ff497dc3762a
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-01-19 16:54:00 +0530

    removed the get request and now express will automatically serves the file on those routes

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,29 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Frontend
 app.use('/', express.static(path.join(__dirname, '../frontend/dist')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname));
-});
 
+// Admin
 app.use('/admin', express.static(path.join(__dirname, '../admin/dist')));
-app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname));
-});
 
+// Client
 app.use('/client', express.static(path.join(__dirname, '../client/dist')));
-app.get('/client', (req, res) => {
-  res.sendFile(path.join(__dirname));
-});
+
+// app.use('/', express.static(path.join(__dirname, '../frontend/dist')));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname));
+// });
+
+// app.use('/admin', express.static(path.join(__dirname, '../admin/dist')));
+// app.get('/admin', (req, res) => {
+//   res.sendFile(path.join(__dirname));
+// });
+
+// app.use('/client', express.static(path.join(__dirname, '../client/dist')));
+// app.get('/client', (req, res) => {
+//   res.sendFile(path.join(__dirname));
+// });
 
 app.use((req, res) => {
   res.send("404 page")
