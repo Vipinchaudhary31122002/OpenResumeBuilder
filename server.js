dcf2e20ff0708ac9c9764570acb1f317fae17bff
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-01-25 12:33:15 +0530

    completed the task of serving in react from express server

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
+// import "dotenv/config";
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT;
 
-// Frontend
-app.use('/', express.static(path.join(__dirname, '../frontend/dist')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+
+// LandingPage
+app.use("/", express.static(path.join(__dirname, "./apps/landingpage/dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./apps/landingpage/dist/index.html"));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
