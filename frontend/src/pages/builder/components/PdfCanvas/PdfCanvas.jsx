95c67ddfd379521083c8a70b672a788ff8b20696
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-04-01 03:52:05 +0530

    testing of usepdf hook in react

--- frontend/src/pages/builder/components/PdfCanvas/PdfCanvas.jsx
+++ frontend/src/pages/builder/components/PdfCanvas/PdfCanvas.jsx
@@ -5,55 +5,91 @@ import {
   Document,
   StyleSheet,
   PDFViewer,
+  // usePDF,
 } from "@react-pdf/renderer";
-// import { useSelector } from "react-redux";
-
+import { useSelector } from "react-redux";
+// import { useEffect } from "react";
 const styles = StyleSheet.create({
   page: {
     flexDirection: "row",
-    backgroundColor: "#E4E4E4",
+    backgroundColor: "white",
   },
   section: {
+    flexDirection: "column",
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
 });
 
-const PdfDoc = (
-  <Document
-    options={{ workerSrc: "/pdf.worker.js" }}
-    onLoadError={(error) => console.log("Error while loading PDF:", error)}
-  >
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Resume Template Section</Text>
-      </View>
-    </Page>
-  </Document>
-);
+// const PdfDoc = () => {
+//   const { fullname, email, phonenumber, address, headline } = useSelector(
+//     (state) => state.resume.initialresume
+//   );
+//   return (
+//     <Document>
+//       <Page size="A4" style={styles.page}>
+//         <View style={styles.section}>
+//           <Text>
+//             Resume Template Section
+//             {fullname}
+//             {headline}
+//             {email}
+//             {phonenumber}
+//             {address}
+//           </Text>
+//         </View>
+//       </Page>
+//     </Document>
+//   );
+// };
 
-// const { fullname, email, phonenumber, address, headline } = useSelector(
-//   (state) => state.resume.initialresume
-{
-  /* {fullname}
-<br />
-{headline}
-<br />
-{email}
-<br />
-{phonenumber}
-<br />
-{address} */
-}
 const PdfCanvas = () => {
+  const { fullname, email, phonenumber, address, headline } = useSelector(
+    (state) => state.resume.initialresume
+  );
+  // const MyDoc = (
+  //   <Document>
+  //     <Page size="A4" style={styles.page}>
+  //       <View style={styles.section}>
+  //         <Text>Resume Template</Text>
+  //         <Text>{fullname}</Text>
+  //         <Text>{headline}</Text>
+  //         <Text>{email}</Text>
+  //         <Text>{phonenumber}</Text>
+  //         <Text>{address}</Text>
+  //       </View>
+  //     </Page>
+  //   </Document>
+  // );
+  // const [instance, updateInstance] = usePDF({ document: MyDoc });
+  // updateInstance();
+
   return (
     <>
-    <PDFViewer>
-      <PdfDoc/>
-    </PDFViewer>
+      <PDFViewer width="100%" height="100%">
+        <Document>
+          <Page size="A4" style={styles.page}>
+            <View style={styles.section}>
+              <Text>Resume Template Section</Text>
+              <Text>{fullname}</Text>
+              <Text>{headline}</Text>
+              <Text>{email}</Text>
+              <Text>{phonenumber}</Text>
+              <Text>{address}</Text>
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
     </>
   );
+
+  // return (
+  //   // <PDFViewer>
+
+  //   //   {instance.url}</PDFViewer>
+  //   <iframe src={instance.url} height="100%" width="100%"></iframe>
+  // );
 };
 
 export default PdfCanvas;
