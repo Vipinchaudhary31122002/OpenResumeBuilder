c715902d3a7157a76a7a938f1c423944438d2cd7
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-04-03 12:06:52 +0530

    finally figure out way to create pdf and render it app as images

--- frontend/src/pages/builder/components/PdfCanvas/PdfCanvas.jsx
+++ frontend/src/pages/builder/components/PdfCanvas/PdfCanvas.jsx
@@ -4,92 +4,68 @@ import {
   View,
   Document,
   StyleSheet,
-  PDFViewer,
-  // usePDF,
+  usePDF,
 } from "@react-pdf/renderer";
+import { Document as ViewDoc, Page as ViewPage, pdfjs } from "react-pdf";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-// import { useEffect } from "react";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
 const styles = StyleSheet.create({
   page: {
     flexDirection: "row",
-    backgroundColor: "white",
+    backgroundColor: "#E4E4E4",
+    color: "black",
   },
   section: {
-    flexDirection: "column",
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
 });
 
-// const PdfDoc = () => {
-//   const { fullname, email, phonenumber, address, headline } = useSelector(
-//     (state) => state.resume.initialresume
-//   );
-//   return (
-//     <Document>
-//       <Page size="A4" style={styles.page}>
-//         <View style={styles.section}>
-//           <Text>
-//             Resume Template Section
-//             {fullname}
-//             {headline}
-//             {email}
-//             {phonenumber}
-//             {address}
-//           </Text>
-//         </View>
-//       </Page>
-//     </Document>
-//   );
-// };
-
 const PdfCanvas = () => {
+  const data = useSelector((state) => state.resume.initialresume);
   const { fullname, email, phonenumber, address, headline } = useSelector(
     (state) => state.resume.initialresume
   );
-  // const MyDoc = (
-  //   <Document>
-  //     <Page size="A4" style={styles.page}>
-  //       <View style={styles.section}>
-  //         <Text>Resume Template</Text>
-  //         <Text>{fullname}</Text>
-  //         <Text>{headline}</Text>
-  //         <Text>{email}</Text>
-  //         <Text>{phonenumber}</Text>
-  //         <Text>{address}</Text>
-  //       </View>
-  //     </Page>
-  //   </Document>
-  // );
-  // const [instance, updateInstance] = usePDF({ document: MyDoc });
-  // updateInstance();
+  const PdfDoc = (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>Resume Template Section</Text>
+          <Text>{fullname}</Text>
+          <Text>{headline}</Text>
+          <Text>{email}</Text>
+          <Text>{phonenumber}</Text>
+          <Text>{address}</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+  const [instance, updateInstance] = usePDF({ document: PdfDoc });
+  // const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
 
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  // }
+  useEffect(() => {
+    updateInstance(PdfDoc);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, updateInstance]);
   return (
     <>
-      <PDFViewer width="100%" height="100%">
-        <Document>
-          <Page size="A4" style={styles.page}>
-            <View style={styles.section}>
-              <Text>Resume Template Section</Text>
-              <Text>{fullname}</Text>
-              <Text>{headline}</Text>
-              <Text>{email}</Text>
-              <Text>{phonenumber}</Text>
-              <Text>{address}</Text>
-            </View>
-          </Page>
-        </Document>
-      </PDFViewer>
+      <ViewDoc file={instance.url} >
+        <ViewPage pageNumber={pageNumber} width={400} />
+      </ViewDoc>
     </>
   );
-
-  // return (
-  //   // <PDFViewer>
-
-  //   //   {instance.url}</PDFViewer>
-  //   <iframe src={instance.url} height="100%" width="100%"></iframe>
-  // );
 };
 
 export default PdfCanvas;
