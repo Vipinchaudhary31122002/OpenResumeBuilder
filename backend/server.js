7a7554508e18a6031c266d79477d97850162078e
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-04-04 10:09:50 +0530

    added code in server.js for production and developement port

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ import { PORT } from "./src/constants.js";
 // import connectDB from "./src/db/connectdb.js";
 import app from "./src/app.js";
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`);
-});
+
+if (process.env.NODE_ENV === "production") {
+  app.listen(PORT, () => {
+    console.log(`OpenResumeBuilder is listening on port ${PORT}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`OpenResumeBuilder is listening on port http://localhost:${PORT}`);
+  });
+}
 // connectDB()
 //   .then(() => {
 //     app.listen(DEVELOPMENT_PORT, () => {
